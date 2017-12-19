db4b99ca65e0c799e668b4d9476f14c867474726
damian <damian12525@gmail.com>
2017-12-19 14:12:26 +0100

    PackageSender

--- PackageSender.cpp
+++ PackageSender.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "PackageSender.h"
+
+
+PackageSender::PackageSender(ElementID _senderID) {
+
+}
+
+ReceiverPreferences PackageSender::getReceiverPreferences() {
+    return ReceiverPreferences();
+}
+
+void PackageSender::setReceiverPrefereneces(ReceiverPreferences _preferences_to_set) {
+
+}
+
+void PackageSender::sendPackage() {
+
+}
+
+std::vector<Package> PackageSender::getSendingBuffer() {
+    return std::vector<Package>();
+}
+
+
+
+
--- PackageSender.h
+++ PackageSender.h
@@ -6,8 +6,22 @@
 #define NET_SIMULATION_PACKAGESENDER_H
 
 
-class PackageSender {
+#include <vector>
+#include "ReceiverPreferences.h"
+#include "Package.h"
+#include "ElementID.h"
 
+class PackageSender {
+    ReceiverPreferences receiverPreferences;
+    std::vector<Package>sendingBuffer;
+
+public:
+
+    PackageSender (ElementID _senderID);
+    ReceiverPreferences getReceiverPreferences();
+    void setReceiverPrefereneces (ReceiverPreferences _preferences_to_set);
+    void sendPackage();
+    std::vector<Package> getSendingBuffer();
 };
 
 
