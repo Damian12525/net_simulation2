4416be20d4c5f64f843ba9b14d68b24b01783738
damian <damian12525@gmail.com>
2017-12-19 14:20:34 +0100

    ReceiverPreferences

--- ReceiverPreferences.cpp
+++ ReceiverPreferences.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "ReceiverPreferences.h"
+
+std::map<IPackageReceiver *, double> ReceiverPreferences::getProbabilities() {
+    return std::map<IPackageReceiver *, double>();
+}
+
+void ReceiverPreferences::setProbabilities(std::map<IPackageReceiver *, double>) {
+
+}
+
+void ReceiverPreferences::addReceiver(IPackageReceiver *new_receiver) {
+
+}
+
+void ReceiverPreferences::addReceiverWithProbability(IPackageReceiver *new_receiver, double probability) {
+
+}
+
+void ReceiverPreferences::removeReceiver(IPackageReceiver *) {
+
+}
+
+IPackageReceiver *ReceiverPreferences::drawReceiver() {
+    return nullptr;
+}
+
+std::pair<IPackageReceiver *, double> *ReceiverPreferences::view() {
+    return nullptr;
+}
--- ReceiverPreferences.h
+++ ReceiverPreferences.h
@@ -6,7 +6,22 @@
 #define NET_SIMULATION_RECEIVERPREFERENCES_H
 
 
+#include <map>
+#include "IPackageReceiver.h"
+
 class ReceiverPreferences {
+
+    std::map<IPackageReceiver*,double > probabilities;
+
+public:
+    std::map<IPackageReceiver*,double > getProbabilities();
+    void setProbabilities (std::map<IPackageReceiver*,double >);
+    void addReceiver(IPackageReceiver* new_receiver);
+    void addReceiverWithProbability(IPackageReceiver* new_receiver, double probability);
+    void removeReceiver (IPackageReceiver*);
+    IPackageReceiver* drawReceiver();
+    std::pair<IPackageReceiver*, double >* view ();
+
 
 };
 
